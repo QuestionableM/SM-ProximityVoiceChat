c7711feb19df6c67e32797c6708317cc560733dd
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2026-07-08 10:05:40 -0700

    Minor SMSDK update

--- Dependencies/SmSdk/include/SmSdk/Gui/OptionsItemSlider.hpp
+++ Dependencies/SmSdk/include/SmSdk/Gui/OptionsItemSlider.hpp
@@ -30,11 +30,11 @@ class OptionsItemSlider : public OptionsItemBase
 	SDK_PUB SMSDK_API void update() override;
 #endif
 	
-	/* 0x0010 */ SDK_PUB MyGUI::ScrollBar* m_pSlider;
-	/* 0x0018 */ SDK_PUB MyGUI::TextBox* m_pValueTextBox;
-	/* 0x0044 */ SDK_PUB float m_fMinValue;
-	/* 0x0048 */ SDK_PUB float m_fMaxValue;
-	/* 0x0050 */ SDK_PUB std::size_t m_uSteps;
+	SDK_PUB MyGUI::ScrollBar* m_pSlider;
+	SDK_PUB MyGUI::TextBox* m_pValueTextBox;
+	SDK_PUB float m_fMinValue;
+	SDK_PUB float m_fMaxValue;
+	SDK_PUB std::size_t m_uSteps;
 };
 
 SMSDK_END_NAMESPACE
